71cfa614c9ea1e2ba29517809437f25cd7ed2492
Muhammad Kamran <134825430+Kami-1716@users.noreply.github.com>
2024-01-01 23:05:58 +0500

    created pages and routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ForgotPassword, Home, Offers, Profile, SignIn, SignUp } from "./pages";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>Hello world</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Offers from "./Offers";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import ForgotPassword from "./ForgotPassword";
+import Profile from "./Profile";
+
+
+export { Home, Offers, SignIn, SignUp, ForgotPassword, Profile };
